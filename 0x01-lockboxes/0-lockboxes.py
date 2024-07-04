7883a9ef811e23d5a6d8c8d531ccf42b90684e4b
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-07-04 17:17:48 +0300

    changes to the file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,41 +1,65 @@
 #!/usr/bin/python3
+"""Solves the lock boxes puzzle """
 
-"""
-Problem: You have n number of locked boxes in front of you.
-         Each box is numbered sequentially from 0 to n - 1
-         and each box may contain keys to the other boxes.
-Task: Write a method that determines if all the boxes can be opened.
-"""
 
-def can_unlock_all(boxes):
+def look_next_opened_box(opened_boxes):
+    """Looks for the next opened box
+    Args:
+        opened_boxes (dict): Dictionary which contains boxes already opened
+    Returns:
+        list: List with the keys contained in the opened box
     """
-    Function that checks if all boxes can be unlocked.
-    Uses a stack for a depth-first search (DFS) approach to track and unlock boxes.
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
+
+
+def canUnlockAll(boxes):
+    """Check if all boxes can be opened
+    Args:
+        boxes (list): List which contain all the boxes with the keys
+    Returns:
+        bool: True if all boxes can be opened, otherwise, False
     """
-    if not isinstance(boxes, list) or len(boxes) == 0:
-        return False
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
 
-    n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    stack = [0]
+    aux = {}
+    while True:
+        if len(aux) == 0:
+            aux[0] = {
+                'status': 'opened',
+                'keys': boxes[0],
+            }
+        keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                try:
+                    if aux.get(key) and aux.get(key).get('status') \
+                       == 'opened/checked':
+                        continue
+                    aux[key] = {
+                        'status': 'opened',
+                        'keys': boxes[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        elif len(aux) == len(boxes):
+            break
+        else:
+            return False
 
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key < n and not unlocked[key]:
-                unlocked[key] = True
-                stack.append(key)
+    return len(aux) == len(boxes)
 
-    return all(unlocked)
 
-# Example usage
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(can_unlock_all(boxes))  # True
+def main():
+    """Entry point"""
+    canUnlockAll([[]])
 
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(can_unlock_all(boxes))  # True
 
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(can_unlock_all(boxes))  # False
+if __name__ == '__main__':
+    main()
