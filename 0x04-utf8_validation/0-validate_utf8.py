9096781813d17cd5272575aaa8c3ae2d80af3ef4
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-07-25 08:27:24 +0300

    changes in code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,48 @@
-#!/usr/bin/python3
-"""
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
-"""
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
-
-
 def validUTF8(data):
     """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
+    Check if the data set represents a valid UTF-8 encoding.
+    
+    :param data: List of integers representing the bytes of the data set
+    :return: True if data is a valid UTF-8 encoding, else False
     """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
+    num_bytes = 0
 
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
+    # Masks to check the most significant bits of a byte
+    mask1 = 1 << 7    # 10000000
+    mask2 = 1 << 6    # 01000000
+
+    for num in data:
+        if num < 0 or num > 255:
             return False
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+        mask = 1 << 7
+        if num_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            while mask & num:
+                num_bytes += 1
+                mask >>= 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
                 return False
-            if byte[0:2] != [1, 0]:
+        else:
+            # Check that the remaining bytes start with 10
+            if not (num & mask1 and not (num & mask2)):
                 return False
-    return True
+
+        num_bytes -= 1
+
+    return num_bytes == 0
+
+if __name__ == "__main__":
+    # Example data
+    data1 = [65]
+    print(validUTF8(data1))  # Expected Output: True
+
+    data2 = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+    print(validUTF8(data2))  # Expected Output: True
+
+    data3 = [229, 65, 127, 256]
+    print(validUTF8(data3))  # Expected Output: False
